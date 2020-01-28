295a38ce148746d903ec07dea5d7ec53e0a8f729
Srikanta <srnagar@microsoft.com>
2020-01-28 13:28:33 -0800

    Fix namespace

--- java/com/jongallant/azure/identity/extensions/DefaultAzureServiceBusCredentialTest.java
+++ java/com/jongallant/azure/identity/extensions/DefaultAzureServiceBusCredentialTest.java
@@ -3,7 +3,7 @@ package com.jongallant.azure.identity.extensions;
 import com.azure.identity.DefaultAzureCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
-import com.microsoft.azure.servicebus.security.DefaultAzureServiceBusCredential;
+import com.jongallant.azure.identity.extensions.DefaultAzureServiceBusCredential;
 import org.junit.Test;
 
 public class DefaultAzureServiceBusCredentialTest {
